e1f1fde4b2b471b855a457bdd421902913711f86
Tafka_4 <shkh071216@gmail.com>
2025-02-13 01:23:38 +0900

    Merge branch 'main' of https://github.com/HICC-2024-2-project-contest/team3 into tafka-dev

--- api/src/app.js
+++ api/src/app.js
@@ -5,6 +5,13 @@ import authRouter from "./router/authRouter.js";
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: 104857600 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "O, Authorization, Accept, Content-Type, Origin, X-Access-Token, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    next();
+});
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auth", authRouter);
@@ -13,6 +20,6 @@ app.get("/", (req, res) => {
     res.send("TRPG BACKEND SAMPLE");
 });
 
-app.listen(5000, () => {
+app.listen(1337, () => {
     console.log("Server is running on port 5000");
 });
