afb5eabb17a07d9873420c098eabd098b8f6ec40
Tafka_4 <shkh071216@gmail.com>
2025-02-15 16:41:02 +0900

    Fix: Search Engine Fixed

--- api/src/utils/masterAgent/LLMCall.js
+++ api/src/utils/masterAgent/LLMCall.js
@@ -1,7 +1,11 @@
 import {} from "@google/generative-ai";
 
 async function LLMCall(prompt) {
-    return prompt;
+    const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+    const result = await model.generateContent(prompt);
+    return result.response.text()
 }
 
 export { LLMCall };
